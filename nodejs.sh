7fc323a884afe45f76c3f8ce31eddc728a0612d2
Aung Nyein Chan Oo <techwithnc.techwithnc@gmail.com>
2023-01-16 13:55:39 +0630

    edited by Aung Nyein Chan Oo

--- nodejs.sh
+++ nodejs.sh
@@ -1,1 +1,26 @@
 # How to install Node.js
+
+sudo apt install -y nodejs
+
+mkdir project01
+cd project01
+vim hello-world.js
+
+[
+const http = require('http');
+
+const hostname = '0.0.0.0';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World! , By Tech With NC');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
+]
+
+node hello-world.js
